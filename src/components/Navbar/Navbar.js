9530d9d3a8d48de2a54f0325dcbfba320da3566f
Nate Worley <immythz@gmail.com>
2021-07-24 03:11:48 -0500

    added LinkedIn and GitHub links and icons to mobile menu. Added moblie menu icon to Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import M from 'materialize-css'
+import { Menu } from '@material-ui/icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import Resume from './Coding_Resume.pdf'
 
 
 export default function Navbar() {
+    useEffect(()=> {
+        let elements = document.querySelectorAll(".sidenav")
+        M.Sidenav.init(elements)
+    }, [])
+
     return (
         <header>
             <nav className="nav-wrapper transparent">
                 <div className="container">
                     <a href="/#" className="brand-logo">Nate Worley</a>
                     <a href="/#" className="sidenav-trigger" data-target="mobile-menu">
-                        <i className="material-icons">menu</i>
+                        <Menu style={{ fontSize: "50", margin: "8 0 0 0" }}/>
                     </a>
                     <ul className="right hide-on-med-and-down">
                         <li><a href="#work">Work</a></li>
@@ -34,6 +41,16 @@ export default function Navbar() {
                         <li><a href="#about-me">About Me</a></li>
                         <li><a href="#work">Work</a></li>
                         <li><a href="#contact">Contact Me</a></li>
+                        <li><a href={Resume} target="_blank" rel="noopener noreferrer"
+                                download="Coding_Resume.pdf">Resume</a>
+                        </li>
+                        <li><a href="https://www.linkedin.com/in/nathaniel-worley-a21030150/" target="_blank" rel="noopener noreferrer"
+                                data-tooltip="LinkedIn"> <FontAwesomeIcon className="fa-3x" icon={faLinkedin} />
+                            </a></li>
+                        <li><a href="https://github.com/ImMythz" target="_blank" rel="noopener noreferrer"
+                                data-tooltip="github">
+                                <FontAwesomeIcon className="fa-3x" icon={faGithub} />
+                            </a></li>
                     </ul>
                 </div>
             </nav>
