0990a4a66cd2e6cb3950ad0ca81c62c9140477b2
Nate Worley <immythz@gmail.com>
2021-12-01 18:05:11 -0600

    added deployed links and fixed project layout

--- src/components/ProjectTiles/ProjectTiles.js
+++ src/components/ProjectTiles/ProjectTiles.js
@@ -27,7 +27,7 @@ export default function ProjectTiles() {
                         <h2 className="blue-text text-darken-2">BarScout</h2>
                     </a>
                     <p>This web application allows you to find bars in different cities, states, and counties. You will also
-                        be able to locate the bars near the searched location on a map.</p>
+                        be able to locate the bars near the searched location on a map. Deployed application <a href="https://immythz.github.io/BarScout/" target="_blank" rel="noopener noreferrer">Here</a>.</p>
                 </div>
             </div>
             <div className="row">
@@ -41,7 +41,7 @@ export default function ProjectTiles() {
                     </a>
                     <p>A weather application that allows you to search for a city and get the daily weather report for that
                         city. You will also be able to get the 5-day forecast of that searched city. Your recent
-                        searches will also be saved.</p>
+                        searches will also be saved. Deployed application <a href="https://immythz.github.io/Weather_Dashboard/" target="_blank" rel="noopener noreferrer">Here</a>.</p>
                 </div>
             </div>
             <div className="row">
@@ -56,44 +56,44 @@ export default function ProjectTiles() {
                     <p>This is a password generator application. The user can choose between different criteria to include
                         in their password and a random password will be generated based on the user selected criteria. You
                         also
-                        have the option to copy your generated password to your clipboard.</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col s12 l4">
-                    <img src={StolenCanvas} alt="Stolen Canvas"
-                        className="responsive-img materialboxed"/>
-                </div>
-                <div className="col s12 l6 offset-l1">
-                    <a href="https://github.com/ImMythz/Stolen-Canvas" target="_blank" rel="noopener noreferrer">
-                        <h2 className="blue-text text-darken-2">Stolen Canvas</h2>
-                    </a>
-                    <p>This web application is a full fledged E-Commerce site that is based on the premise of pseudo-selling famous 
-                        pieces of art as well as MS Paint drawing from the creators of the site. </p>
+                        have the option to copy your generated password to your clipboard. Deployed application <a href="https://immythz.github.io/Password_Generator/" target="_blank" rel="noopener noreferrer">Here</a>.</p>
                 </div>
             </div>
             <div className="row">
                 <div className="col s12 l4 offset-l1 push-l7">
-                    <img src={NoteTaker} alt="Note Taker Screenshot"
+                    <img src={StolenCanvas} alt="Stolen Canvas"
                         className="responsive-img materialboxed"/>
                 </div>
                 <div className="col s12 l6 offset-l1 pull-l5 right-align">
-                    <a href="https://github.com/ImMythz/Note_Taker" target="_blank" rel="noopener noreferrer">
-                        <h2 className="blue-text text-darken-2">Note Taker</h2>
+                    <a href="https://github.com/ImMythz/Stolen-Canvas" target="_blank" rel="noopener noreferrer">
+                        <h2 className="blue-text text-darken-2">Stolen Canvas</h2>
                     </a>
-                    <p>This application Note Taker application allows the user to write, save, and delete notes to help them organize and keep track of thoughts and tasks.</p>
+                    <p>This web application is a full fledged E-Commerce site that is based on the premise of pseudo-selling famous 
+                        pieces of art as well as MS Paint drawing from the creators of the site. Deployed application <a href="https://afternoon-ocean-89274.herokuapp.com/" target="_blank" rel="noopener noreferrer">Here</a>.</p>
                 </div>
             </div>
             <div className="row">
                 <div className="col s12 l4">
-                    <img src={TechBlog} alt="Tech Blog Screenshot"
+                    <img src={NoteTaker} alt="Note Taker Screenshot"
                         className="responsive-img materialboxed"/>
                 </div>
                 <div className="col s12 l6 offset-l1">
+                    <a href="https://github.com/ImMythz/Note_Taker" target="_blank" rel="noopener noreferrer">
+                        <h2 className="blue-text text-darken-2">Note Taker</h2>
+                    </a>
+                    <p>This application Note Taker application allows the user to write, save, and delete notes to help them organize and keep track of thoughts and tasks. Deployed application <a href="https://note-taking-app-express.herokuapp.com/" target="_blank" rel="noopener noreferrer">Here</a>.</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col s12 l4 offset-l1 push-l7">
+                    <img src={TechBlog} alt="Tech Blog Screenshot"
+                        className="responsive-img materialboxed"/>
+                </div>
+                <div className="col s12 l6 offset-l1 pull-l5 right-align">
                     <a href="https://github.com/ImMythz/Tech_Blog" target="_blank" rel="noopener noreferrer">
                         <h2 className="blue-text text-darken-2">Tech Blog</h2>
                     </a>
-                    <p>This is a Tech Blog application that creates a CMS-style blog site. This site allows users to create an account, sign in to an existing account, leave posts, edit posts, delete posts, and comment on posts.</p>
+                    <p>This is a Tech Blog application that creates a CMS-style blog site. This site allows users to create an account, sign in to an existing account, leave posts, edit posts, delete posts, and comment on posts. Deployed application <a href="https://young-woodland-81817.herokuapp.com/" target="_blank" rel="noopener noreferrer">Here</a>.</p>
                 </div>
             </div>
         </section>
